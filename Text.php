d0b62b25efa2f04b7c320f6f3378213b1a594adf
Rolf K <abstractstar@gmail.com>
2018-04-24 22:49:55 +0200

    removed some dots

--- Text.php
+++ Text.php
@@ -44,9 +44,9 @@ $quest_over_variations = [
 $win_variations = [
     [ "your quest is over.", "we present you a new quest.", "push button b", "to select a world" ],
     [ "  A magician was   ", "driving down the road. Then", "  he turned  ", "into a driveway!!" ],
-    [ " This is my step-  ", "  ladder. I never knew my  ", " real ladder.", "                 " ],
+    [ "  I have a step-   ", "  ladder. I never knew my  ", " real ladder.", "                 " ],
     [ " A blind man walks ", "        into a bar.        ", " And a table.", "   And a chair.  " ],
-    [ "my dog can do magic", "      tricks. he is a      ", "     ...     ", " labracadabrador." ],
+    [ "my dog can do magic", "      tricks. he is a      ", "             ", " labracadabrador." ],
     [ "   I used to be    ", " addicted to soap, but now ", "I am clean...", "                 " ],
     [ "I poured root beer ", "into a square glass. Now I ", "             ", " just have beer. " ],
     [ "     Time flies    ", "       like an arrow.      ", " Fruit flies ", "  like a banana! " ],
