f9a94219f4ad1f539390ec13651128a860f83440
Rolf K <abstractstar@gmail.com>
2018-11-10 20:49:45 +0100

    Version change in honor of underground bonus area shuffling!
    Yes, it really works! No bugs seen in this regard (yet).

--- app/Randomizer.php
+++ app/Randomizer.php
@@ -55,7 +55,7 @@ class Randomizer
     private $log;
     // TODO: move all enemy data to Enemy class
     public $enemy_pools;
-    const VERSION = "0.8.8";
+    const VERSION = "0.9";
 
     // Color schemes. TODO: improve
     public $colorschemes = [];
