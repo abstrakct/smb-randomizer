396d3f3342759e5a321b9c55c53b50d2710e1423
Rolf K <abstractstar@gmail.com>
2020-01-28 18:50:37 +0100

    v0.9.8 beta

--- app/Randomizer.php
+++ app/Randomizer.php
@@ -57,7 +57,7 @@ class Randomizer
     // TODO: move all enemy data to Enemy class
     public $enemy_pools;
     public $item_pools;
-    const VERSION = "0.9.7";
+    const VERSION = "0.9.8 beta";
 
     // Color schemes. TODO: improve
     public $colorschemes = [];
